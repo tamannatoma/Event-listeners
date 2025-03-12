e514a6eeab992320117ee316aa2e32eba7ee23dc
tamanna <tamannabegum646@gmail.com>
2025-03-12 01:15:47 +0100

    comment

---
+++ main.js
@@ -0,0 +1,15 @@
+let zoomINbtn = document.querySelector(".zoom-in-btn");
+let zoomOutbtn = document.querySelector(".zoom-out-btn");
+let img = document.querySelector("#image");
+
+function zoomIn() {
+    img.width += 100;
+}
+
+
+function zoomOut() {
+    img.width -= 100;
+}
+
+zoomINbtn.addEventListener("click", zoomIn);
+zoomOutbtn.addEventListener("click", zoomOut);
